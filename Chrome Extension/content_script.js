4b1e4ba008943ace09b1039fa2416a02f466fad2
cgulotta <charliegulotta@gmail.com>
2016-04-18 18:21:07 -0400

    Added censorship functionality
    100 most popular nouns now replaced with ‘banana’

--- Chrome Extension/content_script.js
+++ Chrome Extension/content_script.js
@@ -35,13 +35,17 @@ function walk(node)
 	}
 }
 
+
+
 function handleText(textNode)
 {
+  var noun_arr = Array("time","issue","year","side","people","kind","way","head","day","house","man","service","thing","friend","woman","father","life","power","child","hour","world"	,"game","school"	,"line","state"	,"end","family"	,"member","student"	,"law","group"	,"car","country"	,"city","problem"	,"community","hand"	,"name","part"	,"president","place"	,"team","case"	,"minute","week"	,"idea","company"	,"kid","system"	,"body","program"	,"information","question"	,"back","work"	,"parent","government" ,"face","number"	,"others","night"	,"level","Mr"	,"office","point"	,"door","home"	,"health","water"	,"person","room"	,"art","mother"	,"war","area"	,"history","money"	,"party","storey"	,"result","fact"	,"change","month"	,"morning","lot"	,"reason","right"	,"research","study"	,"girl","book"	,"guy","eye"	,"food","job"	,"moment","word"	,"air","business"	,"teacher");
+
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bMaryland\b/g, "Banana");
-	v = v.replace(/\bMARYLAND\b/g, "BANANA");
-	v = v.replace(/\bMarylands\b/g, "Bananas");
+  for (var i = 0; i < noun_arr.length; i++){
+    v = v.replace(noun_arr[i], "banana");
+  }
 
 	textNode.nodeValue = v;
 }
